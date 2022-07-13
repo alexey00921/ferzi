03268bdeba03a37cfdc4aca9789e6fd4d06646fd
alexey00921 <gdal_kamag@mail.ru>
2022-07-13 22:54:15 +0300

    second commit

--- 8_ferzei/_8_ferzei.py
+++ 8_ferzei/_8_ferzei.py
@@ -28,3 +28,47 @@ def rhombus(chessboard):
 
 
 
+def rotation(chessboard):
+    new_chessboard = empty_chessboard(8)
+    for x, line in enumerate(chessboard):
+        for y, i in enumerate(line):
+            new_chessboard[y][7 - x] = i
+    return new_chessboard
+
+
+
+def main(*args):
+    chessboard = empty_chessboard(8)
+
+
+    for i in args:
+        chessboard[i[0] - 1][i[1] - 1] = 1
+
+    print_chesboard(chessboard)
+
+    for _ in range(2):
+        if not line_sum(chessboard):
+            print('NO')
+            sys.exit('Больше одного ферзя на строке или столбце')
+
+        if not line_sum(rhombus(chessboard)):
+            print('NO')
+            sys.exit('Больше одного ферьзя по диагонали')
+
+        chessboard = rotation(chessboard)
+
+    print('YES')
+
+
+
+a1 = (1, 2)
+a2 = (2, 4)
+a3 = (3, 6)
+a4 = (4, 8)
+a5 = (5, 3)
+a6 = (6, 1)
+a7 = (7, 7)
+a8 = (8, 5)
+
+
+main(a1, a2, a3, a4, a5, a6, a7, a8)
